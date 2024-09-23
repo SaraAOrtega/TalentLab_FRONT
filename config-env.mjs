f29ff129334db6e5f6bbfd3863f423626b4a355d
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-09-23 18:05:40 +0200

    producción

--- config-env.mjs
+++ config-env.mjs
@@ -8,27 +8,21 @@ const __dirname = path.dirname(__filename);
 const mapboxConfigPath = path.join(__dirname, 'src', 'app', 'environments', 'mapbox-config.ts');
 
 try {
-  // Asegúrate de que el directorio existe
-  const dir = path.dirname(mapboxConfigPath);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-    console.log(`Directorio creado: ${dir}`);
+  console.log('Iniciando configuración de Mapbox');
+  
+  let content = fs.readFileSync(mapboxConfigPath, 'utf8');
+  console.log('Contenido original:', content);
+
+  const mapboxKey = process.env.MAPBOX_KEY;
+  if (!mapboxKey) {
+    throw new Error('MAPBOX_KEY no está definida en las variables de entorno');
   }
 
-  let content;
-  if (fs.existsSync(mapboxConfigPath)) {
-    content = fs.readFileSync(mapboxConfigPath, 'utf8');
-    console.log('Archivo de configuración existente leído.');
-  } else {
-    content = `export const mapboxConfig = {\n  apiKey: '__MAPBOX_KEY__'\n};`;
-    console.log('Nuevo contenido de configuración creado.');
-  }
-
-  const mapboxKey = process.env.MAPBOX_KEY || 'tu_token_de_mapbox_aquí';
-  content = content.replace('__MAPBOX_KEY__', mapboxKey);
-
+  content = content.replace('\'__MAPBOX_KEY__\'', `'${mapboxKey}'`);
+  
   fs.writeFileSync(mapboxConfigPath, content);
-  console.log(`Archivo de configuración de Mapbox actualizado con éxito en: ${mapboxConfigPath}`);
+  console.log('Archivo de configuración de Mapbox actualizado');
+  console.log('Nuevo contenido:', content.replace(mapboxKey, '***TOKEN***'));
 } catch (error) {
   console.error('Error al procesar el archivo de configuración de Mapbox:', error);
   process.exit(1);
