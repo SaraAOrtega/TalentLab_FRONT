0cba3ef11c9a79a388557395041e637f9a998473
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-10-13 12:29:59 +0200

    produccion 1310

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    production: false,
-    endpoint: process.env['NG_APP_API_URL'] || 'http://localhost:3001/'
-  };
+  production: false, 
+  endpoint: 'http://localhost:3001/', 
+  
+ 
+};
